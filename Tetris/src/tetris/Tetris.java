42f7f14209922ab3483abb89a8f35b267fe964ca
Neag Darius Stefan <dariusstefan.neag@ulbsibiu.ro>
2023-08-26 14:09:10 +0300

    hello world

--- Tetris/src/tetris/Tetris.java
+++ Tetris/src/tetris/Tetris.java
@@ -2,12 +2,15 @@
 package tetris;
 
 
+
+
 public class Tetris {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello world");
        
     }
     
