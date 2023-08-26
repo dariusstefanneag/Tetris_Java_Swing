050d95fd54f7755f3d175735367b617904b31524
Neag Darius Stefan <dariusstefan.neag@ulbsibiu.ro>
2023-08-26 14:15:13 +0300

    the real code is starting now

--- Tetris/src/tetris/Tetris.java
+++ Tetris/src/tetris/Tetris.java
@@ -8,7 +8,7 @@ public class Tetris {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("swiched to develop");
+       
     }
     
 }
