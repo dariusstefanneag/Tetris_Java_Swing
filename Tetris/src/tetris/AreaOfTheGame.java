e27fdb97c9c1a6e4cbc063588cb910b0810ee462
Neag Darius Stefan <dariusstefan.neag@ulbsibiu.ro>
2023-08-26 15:24:13 +0300

    added comments for good understanding the class AreaOfTheGame

--- Tetris/src/tetris/AreaOfTheGame.java
+++ Tetris/src/tetris/AreaOfTheGame.java
@@ -4,43 +4,52 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
-
 public class AreaOfTheGame extends JPanel {
+
     private int gridRows;
     private int gridColumns;
     private int gridCellSize;
-    
-    private int[][] shape={{1,0},{1,0},{1,1}};
-    public AreaOfTheGame(JPanel placeholder, int columns){
+
+    // declaring coordonate for L shape
+    private int[][] shape = {{1, 0}, {1, 0}, {1, 1}};
+
+    // method for calling a placeholder where i design it in gameForm
+    public AreaOfTheGame(JPanel placeholder, int columns) {
         placeholder.setVisible(false);
         this.setBounds(placeholder.getBounds());
         this.setBackground(placeholder.getBackground());
         this.setBorder(placeholder.getBorder());
-        
-        gridColumns=columns;
-        gridCellSize=this.getBounds().width / gridColumns;
-        gridRows=this.getBounds().height / gridCellSize;
+
+        gridColumns = columns;
+        gridCellSize = this.getBounds().width / gridColumns;
+        gridRows = this.getBounds().height / gridCellSize;
     }
-    private void drawShape(Graphics g){
-        for(int x=0;x<shape.length;x++){
-            for(int y=0;y<shape[0].length;y++){
-                if(shape[x][y]==1){
+
+    // method to create a shape
+    private void drawShape(Graphics g) {
+        for (int x = 0; x < shape.length; x++) {
+            for (int y = 0; y < shape[0].length; y++) {
+                if (shape[x][y] == 1) {
+                    // setting the color red to the shape
                     g.setColor(Color.red);
-                    g.fillRect(y*gridCellSize, x*gridCellSize, gridCellSize, gridCellSize);
+                    // drawing the shape
+                    g.fillRect(y * gridCellSize, x * gridCellSize, gridCellSize, gridCellSize);
+                    // setting color black for the borders
                     g.setColor(Color.black);
-                    g.drawRect(y*gridCellSize,x*gridCellSize,gridCellSize,gridCellSize);
+                    // seting some borders to the shape
+                    g.drawRect(y * gridCellSize, x * gridCellSize, gridCellSize, gridCellSize);
                 }
             }
         }
     }
 
     @Override
-    protected void paintComponent(Graphics g)
-    {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for(int y =0;y<gridRows;y++){
-            for (int x=0;x<gridColumns;x++){
-                g.drawRect(x*gridCellSize,y*gridCellSize,gridCellSize,gridCellSize);
+        // setting the grid cells for better visualization
+        for (int y = 0; y < gridRows; y++) {
+            for (int x = 0; x < gridColumns; x++) {
+                g.drawRect(x * gridCellSize, y * gridCellSize, gridCellSize, gridCellSize);
             }
         }
         drawShape(g);
